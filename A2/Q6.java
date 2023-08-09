9be6422599a29dae27650748810c7ebadac6225c
Himalay Solanki <99128322+himalayzzz007@users.noreply.github.com>
2023-08-09 21:51:38 +0530

    Update Q6.java

--- A2/Q6.java
+++ A2/Q6.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Q6 {
     public static void main(String args[]){
-        System.out.println("Hello World!")
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int array[]=new int[n];
+        for(int i=0;i<n;i++){
+            array[i]=input.nextInt();
+        }
+        
+        int largest=Integer.MIN_VALUE;
+        int sec_largest=Integer.MIN_VALUE;
+        
+        for(int x : array){
+            if(x>largest){
+                sec_largest=largest;
+                largest=x;
+            }
+            else if (x>sec_largest && x!=largest){
+                sec_largest=x;
+            } 
+        }
+        System.out.println(sec_largest);
     }
 }
