23c6dad4bef58b8bbb17b4920c12919ee6982b26
Himalay Solanki <99128322+himalayzzz007@users.noreply.github.com>
2023-08-09 22:07:26 +0530

    Create Q8.java

---
+++ A2/Q8.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Q7 {
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+        String s = input.nextLine();
+        String reversed = reverseString(s);
+        
+        System.out.println(reversed);
+    }
+    
+    public static String reverseString(String s) {
+        char[] chars = s.toCharArray();
+        int left = 0;
+        int right = chars.length - 1;
+        
+        while (left < right) {
+            char temp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = temp;
+            
+            left++;
+            right--;
+        }
+        
+        return new String(chars);
+    }
+}
