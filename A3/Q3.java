f7ff9f7752274e564115edb5724ba0128f567e44
Himalay Solanki <99128322+himalayzzz007@users.noreply.github.com>
2023-08-11 16:52:50 +0530

    Create Q3.java

---
+++ A3/Q3.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Q3 {
+    String removeDuplicate(String )
+    public static void main(String args[]){
+        Scanner input=new Scanner(System.in);
+        String s=input.nextLine();
+        input.close();
+        for(int i=0; i<s.length(); i++){
+            for(int j=0; j<i; j++){
+                if
+            }
+        }
+    }    
+}
