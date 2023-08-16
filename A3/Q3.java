d2ff7957a2ad85a29e5120b444f17af26f46dc87
Himalay Solanki <99128322+himalayzzz007@users.noreply.github.com>
2023-08-16 14:53:25 +0530

    Update Q3.java

--- A3/Q3.java
+++ A3/Q3.java
@@ -1,14 +1,23 @@
 import java.util.*;
-public class Q3 {
-    String removeDuplicate(String )
-    public static void main(String args[]){
-        Scanner input=new Scanner(System.in);
-        String s=input.nextLine();
-        input.close();
-        for(int i=0; i<s.length(); i++){
-            for(int j=0; j<i; j++){
-                if
+
+public class Q3{
+    public static String removeDuplicates(String input) {
+        Set<Character> seen = new HashSet<>();
+        StringBuilder result = new StringBuilder();
+        for (char ch : input.toCharArray()) {
+            if (!seen.contains(ch)) {
+                seen.add(ch);
+                result.append(ch);
             }
         }
-    }    
+        return result.toString();
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        scanner.close();
+        String result = removeDuplicates(input);
+        System.out.println("String after removing duplicates: " + result);        
+    }
 }
